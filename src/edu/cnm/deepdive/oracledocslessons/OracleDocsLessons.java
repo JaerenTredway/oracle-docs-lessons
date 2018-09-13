fc9f0802825e1f5e8c05b65a7fe4db211a8717c0
jaerentredway <jwilliamtredway@gmail.com>
2018-09-13 17:40:17 -0600

    First commit
    Added a class called OracleDocsLessons.java and wrote in the Javadoc
    comments

---
+++ src/edu/cnm/deepdive/oracledocslessons/OracleDocsLessons.java
@@ -0,0 +1,24 @@
+/**
+ * source file: OracleDocsLessons.java
+ */
+
+package edu.cnm.deepdive.oracledocslessons;
+
+/**
+ * @author will
+ * A class used to complete lessons from the docs.oracle.com Java tutorials.
+ */
+
+public class OracleDocsLessons {
+
+  /**
+   * @param args Command-line parameters (ignored).
+   * 
+   */
+  
+  public static void main(String[] args) {
+    System.out.println("Oracle Docs Lessons");
+
+  }
+
+}
